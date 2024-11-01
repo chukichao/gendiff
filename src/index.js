ef22df78bc3db4a042f585fa173425cbad8848e5
Maxim Chukichev <maxichukichev@gmail.com>
2024-11-01 15:25:06 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
 
   const fileDiff = getDifferentObject(parseFile1, parseFile2);
   const result = createFormat(fileDiff, format);
-  console.log(result);
 
   return result;
 };
